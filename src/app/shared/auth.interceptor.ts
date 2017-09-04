a82d221f2d9040e5c7dddb199175f476101f59cb
turquoise <monica.gosschalk@btinternet.com>
2017-09-04 10:16:48 +0100

    added ngrx-basics

---
+++ src/app/shared/auth.interceptor.ts
@@ -0,0 +1,28 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/switchMap';
+import { Injectable } from '@angular/core';
+
+import { AuthService } from '../auth/auth.service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private store: Store<fromApp.AppState>) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log('Intercepted!', req);
+    // const copiedReq = req.clone({headers: req.headers.set('', '')});
+    //const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
+    return this.store.select('auth')
+      .take(1)
+      .switchMap( (authState: fromAuth.State) => {
+        const copiedReq = req.clone({params: req.params.set('auth', authState.token )});
+        return next.handle(copiedReq);
+      })
+
+    // return null;
+  }
+}
